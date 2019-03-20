0e0150748fb5352d4644475ed3d4618329a70059
Henry Mound <henrymound@gmail.com>
2019-03-20 16:01:17 -0400

    Updated

--- light_on.php
+++ light_on.php
@@ -1,5 +1,5 @@
 <?php
-    if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['someAction']))
+    if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['photonOn']))
     {
         exec("rgb.py");
     }
