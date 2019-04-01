1775b08e3e9c46b123456ea6d1e25892dde68552
Henry Mound <henrymound@gmail.com>
2019-04-01 15:56:35 -0400

    Created a new home page for pi

--- rgb.py
+++ rgb.py
@@ -1,4 +1,14 @@
 import board
 import neopixel
-pixels = neopixel.NeoPixel(board.D18, 30)
-pixels.fill((0, 255, 0))
+import sys
+
+if len(sys.argv) == 4:
+    # print("Got 3 RGB Values!")
+    r = sys.argv[1]
+    g = sys.argv[2]
+    b = sys.argv[3]
+    # print("R: " + r)
+    # print("G: " + g)
+    # print("B: " + b)
+    pixels = neopixel.NeoPixel(board.D18, 30)
+    pixels.fill((r, g, b))
