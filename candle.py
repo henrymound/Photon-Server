6150e6cb4cfbeb2fc64c26a6aaddd8807a4dc490
Henry Mound <henrymound@gmail.com>
2019-04-03 01:18:25 -0400

    Update candle.py

--- candle.py
+++ candle.py
@@ -1,7 +1,15 @@
 import board
 import neopixel
 import sys
+import random
+import time
 
 pixels = neopixel.NeoPixel(board.D18, 30)
 for x in range(1, 30):
+    colors = [(255, 146, 41),
+              (255, 108, 41),
+              (225, 146, 71),
+              (225, 138, 25)]
+    colorIndex = random.randint(0,len(colors))
     pixels[x] = (r, g, b)
+    time.sleep(.05)
