c6b7365c3adef7c76419d1ef07c354245cad8cac
Henry Mound <henrymound@gmail.com>
2019-04-03 01:19:56 -0400

    Update candle.py

--- candle.py
+++ candle.py
@@ -10,6 +10,6 @@ for x in range(1, 30):
               (255, 108, 41),
               (225, 146, 71),
               (225, 138, 25)]
-    colorIndex = random.randint(0,len(colors))
+    colorIndex = random.randint(0,len(colors) - 1)
     pixels[x] = colors[colorIndex]
     time.sleep(.05)
