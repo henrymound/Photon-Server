d1c6302a61343bc599e59998240a2583609d09e8
Henry Mound <henrymound@gmail.com>
2019-04-03 21:23:24 -0400

    Update candle.py

--- candle.py
+++ candle.py
@@ -27,29 +27,20 @@ if len(sys.argv) == 4:
     bUpper = b + TOLERANCE
 
     # Make sure all the values are within the range (0, 255)
-    if(rLower < 0):
-    	rLower = 0
-    if(rUpper > 255):
-    	rUpper = 255
-    if(gLower < 0):
-    	gLower = 0
-    if(gUpper > 255):
-    	gUpper = 255
-    if(bLower < 0):
-    	bLower = 0
-    if(bUpper > 255):
-    	bUpper = 255
+    if(rLower < 0): rLower = 0
+    if(rUpper > 255): rUpper = 255
+    if(gLower < 0): gLower = 0
+    if(gUpper > 255): gUpper = 255
+    if(bLower < 0): bLower = 0
+    if(bUpper > 255): bUpper = 255
 
-	pixels = neopixel.NeoPixel(board.D18, 144, pixel_order=neopixel.RGBW)
-	for x in range(NUMLIGHTS):
-		# Generate an array of random colors based on provided color
-		colors = [(random.randint(gLower,gUpper),
-				 random.randint(rLower,rUpper),
-				 random.randint(bLower,bUpper),
-				 0) for _ in range(NUMCOLORS)]
-	    colorIndex = random.randint(0,len(colors) - 1)
-	    pixelIndex = random.randint(0, NUMLIGHTS - 1)
-	    pixels[pixelIndex] = colors[colorIndex]
-	    time.sleep(.005)
-	# Call script again
-    call(["sudo", "python3", "candle.py", r+"", g+"", b+""])
+    pixels = neopixel.NeoPixel(board.D18, 144, pixel_order=neopixel.RGBW)
+    for x in range(NUMLIGHTS):
+        # Generate an array of random colors based on provided color
+        colors = [(random.randint(gLower,gUpper), random.randint(rLower,rUpper), random.randint(bLower,bUpper), 0) for _ in range(NUMCOLORS)]
+        colorIndex = random.randint(0,len(colors) - 1)
+        pixelIndex = random.randint(0, NUMLIGHTS - 1)
+        pixels[pixelIndex] = colors[colorIndex]
+        time.sleep(.005)
+        # Call script again
+        call(["sudo", "python3", "candle.py", r+"", g+"", b+""])
